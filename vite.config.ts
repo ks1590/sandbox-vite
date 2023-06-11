3f264bc85b92bf22f0189b1c0c5d72ec3a03a312
ks1590 <69412467+ks1590@users.noreply.github.com>
2023-06-11 14:29:36 +0900

    update: Convert localhost to https

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	server: {
-		port: 3000,
+  server: {
+    https: {
+      key: fs.readFileSync('./certs/localhost-key.pem'),
+      cert: fs.readFileSync('./certs/localhost.pem'),
+    },
+  port: 3000,
 	},
 });
