185bd357b4a50c82c0900f1346fca4c8c655b3ea
ks1590 <69412467+ks1590@users.noreply.github.com>
2023-06-25 22:51:44 +0900

    update: navigation menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,25 @@
 const Header = () => {
-	return <div>{import.meta.env.VITE_API_URL}</div>;
+	return (
+		<div className='container'>
+			<ul>
+				<li>
+					<a href='#'>Home</a>
+				</li>
+				<li>
+					<a href='#'>Services</a>
+				</li>
+				<li>
+					<a href='#'>FAQ</a>
+				</li>
+				<li>
+					<a href='#'>About</a>
+				</li>
+				<li>
+					<a href='#'>Contact us</a>
+				</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Header;
